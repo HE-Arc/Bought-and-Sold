6ff8b2552e65f5941098c32e82ba578954154c4d
Edstr <ed.strambini@gmail.com>
2015-03-12 12:57:58 +0000

    Ajout de champs dans le modèle user, ajout du login/logout et l'enregistrement

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get "lots" => "pages#lots"
   get "create_lot" => "pages#create_lot"
   get "about" => "pages#about"
+	
+	get 'users' => 'users#index'
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
