632593220d34508b3de41d0e1d6e766f4af810b2
Edstr <ed.strambini@gmail.com>
2015-03-31 21:58:07 +0000

    Add categories show, index

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,7 @@ Rails.application.routes.draw do
 	resources :lots
 	
 	resources :categories
-	get 'categories' => 'categories#index'
- 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
