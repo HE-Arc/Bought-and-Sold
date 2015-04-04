0a50bb73235eaaa5fc1e4031cf15767af806ebec
PierreChetelat <pierre.chetelat@he-arc.ch>
2015-04-04 17:48:50 +0000

    bug recherche corriger

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -20,9 +20,6 @@ class LotsController < ApplicationController
 	
 	def search
 		@lots = Lot.search(params[:search], current_user.id)
-    respond_to do |format|
-      format.js { render :action => "search", :layout => false }
-    end
 		render '_search_results'
 	end
 
